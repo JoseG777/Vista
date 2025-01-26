0abfad608c53f6ebc71e3909f7a9d23c5488687c
JoseG777 <josegc7773@gmail.com>
2025-01-25 19:29:45 -0500

    map is visible and centered to US

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -3,7 +3,7 @@ import MapComponent from "../../components/Map/Map";
 const Home = () => 
 {
     return (
-        <div style={{ height: "100vh", width: "100%", display: "flex", flexDirection: "column" }}>
+        <div style={{ height: "100%", width: "100%", display: "flex", flexDirection: "column" }}>
             <MapComponent />
         </div>
     )
