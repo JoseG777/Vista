105a9e8a0b7cb81b6b1e507c9ae75d32d283e2e0
JoseG777 <josegc7773@gmail.com>
2025-01-25 22:11:18 -0500

    markers appear on map now

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,12 +1,22 @@
+import React from "react";
 import MapComponent from "../../components/Map/Map";
 
-const Home = () => 
-{
-    return (
-        <div style={{ height: "100%", width: "100%", display: "flex", flexDirection: "column" }}>
-            <MapComponent />
-        </div>
-    )
+const Home: React.FC = () => {
+  const center = { lat: 39.8283, lng: -98.5795 }; 
+
+  const zoom = 13;
+
+  const pins = [
+    { lat: 34.0522, lng: -118.2437 }, 
+    { lat: 40.7128, lng: -74.006 }, 
+    { lat: 37.7749, lng: -122.4194 }, 
+  ];
+
+  return (
+    <div style={{ height: "100%", width: "100%" }}>
+      <MapComponent center={center} zoom={zoom} pins={pins} />
+    </div>
+  );
 };
 
 export default Home;
